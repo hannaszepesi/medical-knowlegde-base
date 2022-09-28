0331c429e175ad12ca630347b1a0be7cca1e47da
Hanna Szepesi <hannaszepesi.7@gmail.com>
2022-09-28 13:45:15 +0200

    create maven project for backend

---
+++ backend/src/main/java/com/codecool/medicalknowledgebase/MedicalKnowledgeBaseApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.medicalknowledgebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalKnowledgeBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalKnowledgeBaseApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/codecool/medicalknowledgebase/MedicalKnowledgeBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.medicalknowledgebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalKnowledgeBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
