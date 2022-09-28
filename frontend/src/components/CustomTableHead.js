24c9f8447c9cb830d42c422f3150096b857d3020
Hanna Szepesi <hannaszepesi.7@gmail.com>
2022-09-28 12:53:18 +0200

    create CustomTableHead.js

---
+++ frontend/src/components/CustomTableHead.js
@@ -0,0 +1,27 @@
+import * as React from "react";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+
+const CustomTableHead = (props) => {
+
+    const {headCells, descriptionArrow} = props
+
+    return (<TableHead>
+        <TableRow>
+            {descriptionArrow === true ? <TableCell
+                key={"description" + headCells.id}
+            >
+            </TableCell> : ""}
+            {headCells.map((headCell) => (<TableCell align="center" sx={{color: "primary", fontWeight: "bold"}}
+                                                     key={headCell.id}
+            >
+                {headCell.label}
+            </TableCell>))}
+            <TableCell></TableCell>
+        </TableRow>
+    </TableHead>);
+
+};
+
+export default CustomTableHead;
