5dbf500ccad5899d5c078f71d1a335a42fb011bb
Hanna Szepesi <hannaszepesi.7@gmail.com>
2022-09-30 12:57:23 +0200

    fix layout of App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,12 @@ import Navbar from "./components/Navbar";
 import {Outlet} from "react-router-dom";
 
 function App() {
-  return (
-    <Box sx={{paddingTop: 5, display: "flex", justifyContent: "center", height: "100vh", alignItems: "center"}}>
-        <Navbar />
-        <Outlet />
-    </Box>
-  );
+    return (
+        <Box sx={{paddingTop: 5, display: "flex", justifyContent: "center", height: "100vh", alignItems: "center"}}>
+            <Navbar/>
+            <Outlet/>
+        </Box>
+    );
 }
 
 export default App;
