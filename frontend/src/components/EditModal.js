bb54ae264735d71a19a08a9b6d77edf2ff394ea8
Hanna Szepesi <hannaszepesi.7@gmail.com>
2022-09-30 12:55:19 +0200

    add handlers and states to EditModal.js to make update and edit work

--- frontend/src/components/EditModal.js
+++ frontend/src/components/EditModal.js
@@ -8,6 +8,9 @@ import {Autocomplete, Button, TextField} from "@mui/material";
 import DeleteIcon from '@mui/icons-material/Delete';
 import AddIcon from "@mui/icons-material/AddCircleOutline";
 import Tooltip from "@mui/material/Tooltip";
+import {useEffect} from "react";
+import {Link} from "react-router-dom";
+import {handleDelete} from "./Row";
 
 const style = {
     position: 'absolute',
@@ -24,46 +27,174 @@ const style = {
     flexDirection: "column"
 };
 
+const fetchNames = async (type) => {
+    const response = await (fetch(`/${type}/names`));
+    return await response.json();
+};
+
+export const sendDataForServer = async (url, payload, method) => {
+    console.log(method)
+    console.log(payload)
+    let response = await fetch(url, {
+        credentials: 'include',
+        method: method,
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify(payload)
+    });
+    console.log(response.status)
+    if (response.status === 200) {
+        console.log(response.json());
+    }
+};
+
 export default function EditModal(props) {
 
-    const {method} = props;
+    const {method, name, row} = props;
+
     const [open, setOpen] = React.useState(false);
+
+    const [riskFactorsList, setRiskFactorsList] = React.useState([]);
+    const [symptomsList, setSymptomsList] = React.useState([]);
+
+    const rowId = method === "edit" ? row.id : "";
+    const [rowName, setRowName] = React.useState(method === "edit" ? row.name : "");
+    const [description, setDescription] = React.useState(method === "edit" ? row.description : "");
+    const [riskFactors, setRiskFactors] = React.useState(method === "edit" ? row.riskFactors : "");
+    const [symptoms, setSymptoms] = React.useState(method === "edit" ? row.symptoms : "");
+
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
+    const submitNew = async (tableName, payload) => {
+        console.log(payload)
+        console.log(tableName)
+        sendDataForServer(`/${tableName.replace(" ", "-") + "s"}/add`, payload, 'POST').then(() => console.log('ok'))
+        window.location.reload();
+
+    }
+
+
+    const submitEdit = async (tableName, payload, id) => {
+        console.log(payload)
+        console.log(tableName)
+        sendDataForServer(`/${tableName.replace(" ", "-") + "s"}/update/${id}`, payload, 'PUT').then(() => console.log('ok'))
+        window.location.reload();
+
+    }
+
+    const handleSubmit = async (tableName, payload, id) => {
+        if (method === "edit") {
+            await submitEdit(tableName, payload, id)
+        }
+        else {
+            await submitNew(tableName, payload)
+        }
+    }
+
+    useEffect(() => {
+        let cancelled = false;
+        fetchNames("risk-factors").then((riskFactors) => {
+            setRiskFactorsList(riskFactors)
+        })
+
+        function cleanUp() {
+            cancelled = true;
+        }
+
+        return cleanUp;
+
+    }, [])
+
+    useEffect(() => {
+        let cancelled = false;
+        fetchNames("symptoms").then((symptoms) => {
+            setSymptomsList(symptoms)
+        })
+
+        function cleanUp() {
+            cancelled = true;
+        }
+
+        return cleanUp;
+
+    }, [])
+
+    const payloadGenerator = (tableName) => {
+        if (tableName === "disease") {
+            return {
+                "name": rowName,
+                "description": description,
+                "symptoms": symptoms,
+                "riskFactors": riskFactors
+            }
+        }
+        else {
+            return {
+                "name": rowName,
+                "description": description,
+            }
+        }
+
+    }
+
+    const nameInput = () => {
+        return (
+            <TextField sx={{margin: 2, maxWidth: 300}}
+                       id="name-input"
+                       label="Name"
+                       onChange={(event) => {
+                           setRowName(event.target.value)
+                       }}
+                       value={rowName}
+            />
+        )
+    }
+
     const risksInput = () => {
         return (
-            <Autocomplete sx={{padding: 2, maxWidth: 300}}
-                          multiple
-                          id="tags-risks"
-                          options={[]}
-                          getOptionLabel={(option) => option.name}
-                          filterSelectedOptions
-                          renderInput={(params) => (
-                              <TextField
-                                  {...params}
-                                  label="Risk factors"
-                              />
-                          )}
-            />
+            <Box display="flex">
+                <Autocomplete sx={{padding: 2, width: 300}}
+                              multiple
+                              id="tags-risk-factors"
+                              options={riskFactorsList}
+                              filterSelectedOptions
+                              onChange={(event, newValue) => {
+                                  setRiskFactors(newValue);
+                              }}
+                              defaultValue={method === "edit" ? riskFactors.map((riskFactor) => riskFactor.name) : undefined}
+                              renderInput={(params) => (
+                                  <TextField
+                                      {...params}
+                                      label="Risk factors"
+                                  />
+                              )}
+                />
+                <IconButton component={Link} to={"/risk-factors"}><AddIcon color="primary"/></IconButton>
+            </Box>
         )
     }
 
     const symptomsInput = () => {
         return (
-            <Autocomplete sx={{padding: 2, maxWidth: 300}}
-                          multiple
-                          id="tags-risks"
-                          options={[]}
-                          getOptionLabel={(option) => option.name}
-                          filterSelectedOptions
-                          renderInput={(params) => (
-                              <TextField
-                                  {...params}
-                                  label="Symptoms"
-                              />
-                          )}
-            />
+            <Box display="flex">
+                <Autocomplete sx={{padding: 2, width: 300}}
+                              multiple
+                              id="tags-symptoms"
+                              options={symptomsList}
+                              filterSelectedOptions
+                              onChange={(event, newValue) => {
+                                  setSymptoms(newValue);
+                              }}
+                              defaultValue={method === "edit" ? symptoms.map((symptom) => symptom.name) : undefined}
+                              renderInput={(params) => (
+                                  <TextField
+                                      {...params}
+                                      label="Symptoms"
+                                  />
+                              )}
+                />
+                <IconButton component={Link} to={"/symptoms"}><AddIcon color="primary"/></IconButton>
+            </Box>
         )
     }
 
@@ -74,17 +205,10 @@ export default function EditModal(props) {
                        label="Description"
                        multiline
                        rows={4}
-                       defaultValue=""
-            />
-        )
-    }
-
-    const nameInput = () => {
-        return (
-            <TextField sx={{margin: 2, maxWidth: 300}}
-                       id="name-input"
-                       label="Name"
-                       defaultValue=""
+                       onChange={(event) => {
+                           setDescription(event.target.value)
+                       }}
+                       value={description}
             />
         )
     }
@@ -92,8 +216,8 @@ export default function EditModal(props) {
     return (
         <React.Fragment>
             {method === "edit" ? <IconButton onClick={handleOpen} color="primary"><EditIcon/></IconButton> :
-                <Tooltip title="Add new disease"><Button onClick={handleOpen} variant="contained"
-                                                         endIcon={<AddIcon/>}>new</Button></Tooltip>}
+                <Tooltip title={"Add new " + name}><Button onClick={handleOpen} variant="contained"
+                                                           endIcon={<AddIcon/>}>new</Button></Tooltip>}
             <Modal
                 open={open}
                 onClose={handleClose}
@@ -103,19 +227,20 @@ export default function EditModal(props) {
                 <Box sx={style}>
                     <Box sx={{display: "flex", justifyContent: "space-between"}}>
                         <Typography padding={2} color="primary" id="modal-modal-title" variant="h4" component="h2">
-                            {method === "edit" ? "Edit" : "Add new"} disease
+                            {method === "edit" ? "Edit" : "Add new"} {name}
                         </Typography>
                         {method === "edit" ?
-                            <Button variant="contained" sx={{height: "fit-content", backgroundColor: "#d32f2f"}}
+                            <Button onClick={() => handleDelete(name, row.id)} variant="contained" sx={{height: "fit-content", backgroundColor: "#d32f2f"}}
                                     startIcon={<DeleteIcon/>}>
                                 Delete
                             </Button> : ""}
                     </Box>
                     {nameInput()}
-                    {risksInput()}
-                    {symptomsInput()}
+                    {name === "disease" ? risksInput() : ""}
+                    {name === "disease" ? symptomsInput() : ""}
                     {descriptionInput()}
-                    <Button sx={{margin: 2}} variant="contained">Save</Button>
+                    <Button sx={{margin: 2}}
+                            variant="contained" onClick={() => handleSubmit(name, payloadGenerator(name), rowId)}>Save</Button>
                 </Box>
             </Modal>
         </React.Fragment>
