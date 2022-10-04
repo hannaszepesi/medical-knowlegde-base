46e70d760cc64f1ed6a8fce99d709927b2539fba
Hanna Szepesi <hannaszepesi.7@gmail.com>
2022-10-04 13:39:24 +0200

    add Util.js

---
+++ frontend/src/components/Util.js
@@ -0,0 +1,47 @@
+import {useEffect} from "react";
+
+export const sendDataForServer = async (url, payload, method) => {
+    await fetch(url, {
+        credentials: 'include',
+        method: method,
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify(payload)
+    });
+};
+
+export const handleDelete = async (tableName, id) => {
+    // eslint-disable-next-line no-restricted-globals
+    let result = confirm("Are you sure you want to delete this?");
+    if (result) {
+        await sendDataForServer(`/${tableName.replace(" ", "-") + "s"}/delete/${id}`, "", 'DELETE');
+        window.location.reload();
+    }
+}
+
+export const SetInitialData = (setterMethod, fetchMethod) => {
+    useEffect(() => {
+        let cancelled = false;
+
+        function updateItems(items) {
+            if (cancelled) return;
+            console.log(items)
+            setterMethod(items)
+        }
+
+        fetchMethod().then((items) => {
+            updateItems(items)
+        })
+        function cleanUp() {
+            cancelled = true;
+        }
+
+        return cleanUp;
+    }, [])
+}
+
+export const getData = async (url) => {
+    const response = await (fetch(url));
+    if (response.status === 200) {
+        return await response.json();
+    }
+}
